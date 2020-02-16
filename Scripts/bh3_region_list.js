1e522fb669c3ff7db8ffe61e17b30aba00a2de52
Mornwind <mornwind95+github@gmail.com>
2020-02-17 02:12:34 +0800

    Update bh3_region_list.js

--- Scripts/bh3_region_list.js
+++ Scripts/bh3_region_list.js
@@ -3,6 +3,8 @@
 By: Mornwind
 
 Surge 4:
+[URL Rewrite]
+^http:\/\/106\.14\.51\.73\/query_gameserver\?version=(\d*\.\d*\.\d*)_gf_ios&t=(\d*)&uid=(\d*) http://106.14.51.73/query_gameserver?version=$1_gf_android&t=$2&uid=$3 302
 [Script]
 http-response ^https:\/\/global1\.bh3\.com\/query_dispatch\?version=(\d*\.\d*\.\d*)_gf_(android|ios|pc)&t=(\d*) requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Mornwind/PublicProfiles/master/Scripts/bh3_region_list.js
 [MITM]
