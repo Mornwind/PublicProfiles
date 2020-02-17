09e322f6b0eaf9397549af9a6e0ff353a5ce29a1
Mornwind <mornwind95+github@gmail.com>
2020-02-17 16:13:42 +0800

    Update BH3_Region_Selector

--- BH3_Region_Selector/bh3_region_list.js
+++ BH3_Region_Selector/bh3_region_list.js
@@ -9,7 +9,15 @@ Surge 4:
 ^http:\/\/139\.224\.7\.27\/query_gameserver\?version=(\d*\.\d*\.\d*)_gf_(android|ios|pc)&t=(\d*)&uid=(\d*) http://139.224.7.27/query_gameserver?version=$1_gf_ios&t=$2&uid=$3 302
 ^http:\/\/106\.15\.162\.73\/query_gameserver\?version=(\d*\.\d*\.\d*)_gf_(android|ios|pc)&t=(\d*)&uid=(\d*) http://106.15.162.73/query_gameserver?version=$1_gf_pc&t=$2&uid=$3 302
 [Script]
-http-response ^https:\/\/global(\d*)\.bh3\.com\/query_dispatch\?version=(\d*\.\d*\.\d*)_gf_(android|ios|pc)&t=(\d*) requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Mornwind/PublicProfiles/master/Scripts/bh3_region_list.js
+http-response ^https:\/\/global(\d*)\.bh3\.com\/query_dispatch\?version=(\d*\.\d*\.\d*)_gf_(android|ios|pc)&t=(\d*) requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Mornwind/PublicProfiles/master/BH3_Region_Selector/bh3_region_list.js
+[MITM]
+hostname = global*.bh3.com
+
+QuanX：
+[rewrite_remote]
+https://raw.githubusercontent.com/Mornwind/PublicProfiles/master/BH3_Region_Selector/bh3_region_rewrite_remote.conf, tag=BH3 Region Rewrite, enabled=true
+[rewrite_local]
+^https:\/\/global(\d*)\.bh3\.com\/query_dispatch\?version=(\d*\.\d*\.\d*)_gf_(android|ios|pc)&t=(\d*) url script-response-body Scripts/bh3_region_list.js
 [MITM]
 hostname = global*.bh3.com
 */
